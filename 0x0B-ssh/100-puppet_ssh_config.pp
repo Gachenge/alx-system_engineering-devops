888d8367da8441bbd14d60135ba0ba3108556863
Gachenge <gachenge1@gmail.com>
2023-03-25 22:59:42 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # code to bypass password
-file{ '/home/victor/programming/alx-system_engineering-devops/0x0B-ssh/100-puppet_ssh_config.pp':
+file{ '/home/victor/.ssh/config':
 owner => 'victor',
 group => 'victor',
 mode => '0600',
