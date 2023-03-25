77263d59232cf84fd112b81cd65dc1dbe359b58d
Gachenge <gachenge1@gmail.com>
2023-03-25 22:46:47 +0300

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,3 @@
-# code to bypass password identification
-file{ '/home/victor/programming/alx-system_engineering-devops/0x0B-ssh/100-puppet_ssh_config.pp':
-owner => 'victor',
-group => 'victor',
-mode => '0600',
-content => "Host ubuntu\n\
-            \tIdentityFile ~/.ssh/school\n\
-            \tPasswordAuthentication no\n",
-}
+Host ubuntu
+            	IdentityFile ~/.ssh/school
+            	PasswordAuthentication no
