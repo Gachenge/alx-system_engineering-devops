60819de34db69c24c9136f8470ddc846d055ef84
Gachenge <gachenge1@gmail.com>
2023-03-26 19:26:29 +0300

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file, give permissions
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => "I love Puppet\n",
+}
